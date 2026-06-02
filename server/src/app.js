59c98ebc34a7706c21db530fce7de0afd274ec57
Siva-2517 <sivasurya6430@gmail.com>
2026-06-02 21:57:06 +0530

    Cors fix 3

--- server/src/app.js
+++ server/src/app.js
@@ -16,26 +16,27 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 
-const cors = require('cors');
+const cors = require("cors");
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigin = process.env.CLIENT_URL;
+const allowedOrigin = process.env.CLIENT_URL;
 
-    // allow requests with no origin (mobile apps, curl, etc.)
-    if (!origin || origin === allowedOrigin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
+app.use(cors({
+  origin: allowedOrigin,
   credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); 
+})); 
 
 // --------------------
 // Middlewares
