d200b5228fcbe726aa31a85cf0c3545360721559
Siva-2517 <sivasurya6430@gmail.com>
2026-06-02 18:31:31 +0530

    cors fixes 2

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+
 const connectDB = require('./config/db');
 const { apiLimiter } = require('./middleware/rateLimiter');
 const authRoutes = require('./routes/authRoutes');
@@ -16,16 +16,26 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 
+const cors = require('cors');
+
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: function (origin, callback) {
+    const allowedOrigin = process.env.CLIENT_URL;
+
+    // allow requests with no origin (mobile apps, curl, etc.)
+    if (!origin || origin === allowedOrigin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // handles preflight requests
-
+app.options("*", cors(corsOptions)); 
 
 // --------------------
 // Middlewares
